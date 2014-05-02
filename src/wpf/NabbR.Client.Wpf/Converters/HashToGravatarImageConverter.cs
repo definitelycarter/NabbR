09723c1cf3f5f28e4c1570f0ba0d57b94c61580b
davepermen <davepermen@hotmail.com>
2014-05-03 00:47:46 +0200

    Update HashToGravatarImageConverter.cs
    Add High DPI Screen Support (a.k.a. load higher definition gravatars)

--- src/wpf/NabbR.Client.Wpf/Converters/HashToGravatarImageConverter.cs
+++ src/wpf/NabbR.Client.Wpf/Converters/HashToGravatarImageConverter.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Globalization;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Data;
 
 namespace NabbR.Converters
@@ -7,12 +9,35 @@ namespace NabbR.Converters
     public class HashToGravatarImageConverter : IValueConverter
     {
         private Uri gravitarUri = new Uri("https://secure.gravatar.com/");
+
+        private static float dpiScaleFactor;
+
+        static HashToGravatarImageConverter()
+        {
+            var dpiProperty = typeof(SystemParameters).GetProperty("Dpi", BindingFlags.NonPublic | BindingFlags.Static);
+            var dpi = (int)dpiProperty.GetValue(null, null);
+
+            dpiScaleFactor = ((float)dpi) / 96f;
+        }
+
+        private static string DpiAdjusted(string size)
+        {
+            float number;
+            if (float.TryParse(size, out number))
+            {
+                return Math.Floor(number * dpiScaleFactor).ToString();
+            }
+            return size;
+        }
+
         public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
         {
-            String hash = value as String;
-            String size = parameter as String;
+            var hash = value as String;
+            var size = parameter as String;
 
-            String relativeUri = String.Format("avatar/{0}?s={1}&d=mm", hash, size);
+            size = DpiAdjusted(size);
+
+            var relativeUri = String.Format("avatar/{0}?s={1}&d=mm", hash, size);
 
             return new Uri(gravitarUri, relativeUri);
         }
