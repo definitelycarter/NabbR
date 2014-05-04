5f64396bf53e6cf7ff92f827f7e2e794c5e59f41
DefinitelyCarter <liamgym4@gmail.com>
2014-05-04 07:45:52 -0400

    Added link to handle direct messages.

---
+++ src/wpf/NabbR.Client.Wpf/Controls/DirectMessageLink.cs
@@ -0,0 +1,32 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using System;
+
+namespace NabbR.Controls
+{
+    class DirectMessageLink : Link
+    {
+        private String username;
+
+        /// <summary>
+        /// Gets or sets the username that sent the direct message.
+        /// </summary>
+        public String Username
+        {
+            get { return this.username; }
+            set
+            {
+                if (username != value)
+                {
+                    username = value;
+                    this.UpdateDisplayName();
+                }
+            }
+        }
+
+        private void UpdateDisplayName()
+        {
+            // todo make displayname show unread messages.
+            this.DisplayName = this.Username;
+        }
+    }
+}
