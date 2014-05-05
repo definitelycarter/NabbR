4bb6c244af15d1d8243bc3737ce75bfe4c4c6a5e
DefinitelyCarter <liamgym4@gmail.com>
2014-05-04 21:50:46 -0400

    squashed 'cross dependent views bug'

--- src/wpf/NabbR.Client.Wpf/Controls/NabbrTextBlock.cs
+++ src/wpf/NabbR.Client.Wpf/Controls/NabbrTextBlock.cs
@@ -1,18 +1,20 @@
 ﻿using FirstFloor.ModernUI.Windows.Controls;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace NabbR.Controls
 {
     public class NabbrTextBlock : TextBlock
     {
         private static String ChatMessagePattern = @"(?<roomlink>#\S*)|(?<mention>@\S*)|(?<hyperlink>(?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'.,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))";
-        
-        private readonly static Regex regex = new Regex(ChatMessagePattern, RegexOptions.Compiled);
+
+        private readonly static Regex regex = new Regex(ChatMessagePattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         static NabbrTextBlock()
         {
@@ -47,8 +49,32 @@ namespace NabbR.Controls
 
             if (this.MessageContent.IndexOf('\n') == -1)
             {
-                String bbcode = regex.Replace(this.MessageContent, OnMatch);
-                this.Inlines.Add(new BBCodeBlock { BBCode = bbcode });
+                MatchCollection matches = regex.Matches(this.MessageContent);
+
+                if (matches.Count == 0)
+                {
+                    this.Inlines.Add(new Run(this.MessageContent));
+                }
+                else
+                {
+                    Int32 start = 0;
+                    foreach (Match match in matches)
+                    {
+                        if (match.Index > start)
+                        {
+                            this.Inlines.Add(new Run(this.MessageContent.Substring(start, match.Index)));
+                            start = match.Index;
+                        }
+
+                        this.AddMatch(match);
+                        start = start + match.Length;
+                    }
+
+                    if (start < this.MessageContent.Length - 1)
+                    {
+                        this.Inlines.Add(new Run(this.MessageContent.Substring(start, this.MessageContent.Length - start)));
+                    }
+                }
             }
             else
             {
@@ -56,24 +82,37 @@ namespace NabbR.Controls
             }
         }
 
-        private String OnMatch(Match match)
+        private void AddMatch(Match match)
         {
             String value = match.Value;
 
             if (match.Groups["hyperlink"].Success)
             {
-                return String.Format("[url={0}]{0}[/url]", value);
+                var text = new Run(value);
+                Hyperlink externalLink = new Hyperlink(text);
+                
+                UriBuilder builder = new UriBuilder(value);
+                externalLink.NavigateUri = builder.Uri;
+                externalLink.RequestNavigate += OnRequestExternalNavigation;
+
+                this.Inlines.Add(externalLink);
             }
             else if (match.Groups["roomlink"].Success)
             {
-                return String.Format("[b][url=/chatroom?room={0}|_self]{1}[/url][/b]", value.Substring(1), value);
+                //this.Inlines.Add(new Hyperlink(new Run(value) { FontWeight = FontWeights.Bold }));
+                this.Inlines.Add(new Run(value));
             }
             else if (match.Groups["mention"].Success)
             {
-                return String.Format("[b][i]{0}[/i][/b]", value);
+                var run = new Run(value);
+                run.FontWeight = FontWeights.Bold;
+                this.Inlines.Add(new Italic(run));
             }
+        }
 
-            return value;
+        private void OnRequestExternalNavigation(Object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.OriginalString);
         }
     }
 }
